fdb72a990cf38116642d96a7a317fb9cc3e7d4b7
danielearcaleni <arcaleni.daniele1@franchettisalviani.net>
2026-03-09 09:49:42 +0100

    primoCommit

---
+++ ProgettoGDR/src/progettogdr/Personaggio.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progettogdr;
+
+/**
+ *
+ * @author arcaleni.daniele2
+ */
+public class Personaggio {
+    private String nome;
+    private int attacco;
+    private int fame;
+    private int sete;
+    private int salute;
+    
+    public Personaggio(String nome, int attacco, int fame, int sete, int salute){
+        this.nome = nome;
+        this.attacco = attacco;
+        this.fame = fame;
+        this.sete = sete;
+        this.salute = salute;
+    }
+    
+    public void aumentaAttacco(int attacco){
+        this.attacco += attacco;
+    }
+    
+    public void bevi(int disseta){
+        this.sete += disseta;
+    }
+    
+    public void mangia(int sfama){
+        this.fame += sfama;
+    }
+    
+    public void perdiVita(int vitaPersa){
+        this.salute += vitaPersa;
+    }
+    
+    public void aumentaVita(int vitaAumentata){
+        this.salute += vitaAumentata;
+    }
+    
+    public void abilitaSpeciale(){
+        //completala
+    }
+}
